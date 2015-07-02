db8d724a1c679143016444b44785f3825c613378
kutiman <kutiman@gmail.com>
2015-07-02 22:14:04 +0300

    first

---
+++ scrBomb.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+private var rotateSpeed = 100.0;
+private var moveSpeed = 1.0;
+
+function Start () {
+	RandomSpeeds();
+}
+
+function Update () {
+	Rotate(rotateSpeed);
+	Move(moveSpeed);
+}
+
+function Rotate (speed : float) {
+	transform.Rotate(0, 0, Time.deltaTime * speed);
+}
+
+function Move (speed : float) {
+	transform.position.x -= speed * Time.deltaTime;
+}
+
+function RandomSpeeds () { //random movement speed and rotation speed
+	rotateSpeed = 30.0 + 150.0 * Random.Range(0.0, 1.0);
+	moveSpeed = 0.8 + 1.0 * Random.Range(0.0, 1.0);
+}
+
+function BlowUp () {
+	Destroy(gameObject);
+}
---
+++ scrGirraffe.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+private var spinSpeed = 60.0;
+private var moveSpeed = 2.4
+;
+private var screenWidth = 4.8;
+private var screenHeight = 3.2;
+
+function Start () {
+	//screenWidth = 4.8 - (collider.bounds.size.y / 2);
+	//screenHeight = 3.2 - (collider.bounds.size.y / 2);;
+}
+
+function Update () {
+	Spin(spinSpeed);
+	Move();
+}
+
+function Spin (speed : float) {
+	transform.Rotate(0, 0, Time.deltaTime * speed);
+}
+
+function Move () {
+	if (Input.GetKey (KeyCode.UpArrow) && transform.position.y < screenHeight) {transform.position.y += moveSpeed * Time.deltaTime;}
+	else if (Input.GetKey (KeyCode.DownArrow) && transform.position.y > -screenHeight) {transform.position.y -= moveSpeed * Time.deltaTime;}
+	if (Input.GetKey (KeyCode.RightArrow) && transform.position.x < screenWidth) {transform.position.x += moveSpeed * Time.deltaTime;}
+	else if (Input.GetKey (KeyCode.LeftArrow) && transform.position.x > -screenWidth) {transform.position.x -= moveSpeed * Time.deltaTime;}
+}
+
+function OnCollisionEnter(coll : Collision) {
+	if (coll.gameObject.tag == "tagBomb") {
+		Debug.Log ("I've collided with a bomb");
+		coll.gameObject.GetComponent(scrBomb).BlowUp();
+	}
+}
+
+
+
