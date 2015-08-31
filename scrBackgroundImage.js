01aecc5ebc6b01ef289d1303dc338a1796874155
kutiman <kutiman@gmail.com>
2015-08-31 14:55:39 +0300

    moving background

---
+++ scrBackgroundImage.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+private var moveSpeed : float = -1.0;
+
+function Start () {
+
+}
+
+function Update () {
+	transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+	if (transform.localPosition.x <= -12.8) {
+		transform.localPosition.x += 25.6;
+	}
+}
