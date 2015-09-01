53b9c4495b12ba5f546245d284729d4f9aa6752a
kutiman <kutiman@gmail.com>
2015-09-01 17:16:27 +0300

    levels controller

---
+++ scrLevels.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var roundActive : boolean = false;
+var round : int = 0;
+var roundOneHazards = new ArrayList();
+
+function Start () {
+	roundOneHazards.Add(function () { TenBombs(); });
+	var obj : Function = roundOneHazards[0]();
+	
+}
+
+function Update () {
+
+}
+
+function CreateHazard (name : String, amount : int) {
+	for (var i = 0; i < amount; i++) {
+		var obj = GameObject.Instantiate(Resources.Load("Prefabs/" + name)) as GameObject;
+	}
+}
+
+function StartRound () {
+	roundActive = true;
+}
+
+function EndRound () {
+	round++;
+	roundActive = false;
+}
+
+// 10 bombs in 10 seconds
+function TenBombs () {
+	StartRound();
+	var n : int = 10;
+	for (var i = 0; i < n; i++) {
+		CreateHazard("objBomb", 1);
+		yield WaitForSeconds(1.0);
+	}
+}
+
+
+
+
