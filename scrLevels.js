e2ce6d1a42188a18628c082afe74eee386531ab4
kutiman <kutiman@gmail.com>
2015-09-05 23:13:39 +0300

    more

--- scrLevels.js
+++ scrLevels.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 
 function Start () {
-	Level2();
+	Level3();
 }
 
 function Update () {
@@ -95,6 +95,40 @@ function Level2 () {
 	
 	CreateHazard("objUfo", 3, 3);
 	CreateHazard("objSmokeGrenade", 2, 2);
+	yield WaitForSeconds(15);
+	
+}
+
+function Level3 () {
+	yield WaitForSeconds(3);
+	
+	CreateHazard("objUfo", 3, 3);
+	CreateHazard("objBomb", 5, 10);
+	CreateHazard("objRocket", 5, 10);
+	CreateHazard("objGrenade", 5, 10);
+	CreateHazard("objWeight", 5, 10);
+	yield WaitForSeconds(15);
+	
+	CreateHazard("objUfo", 6, 6);
+	CreateHazard("objBomb", 10, 10);
+	CreateHazard("objRocket", 10, 10);
+	CreateHazard("objGrenade", 10, 10);
+	CreateHazard("objWeight", 10, 10);
+	yield WaitForSeconds(15);
+	
+	CreateHazard("objUfo", 10, 10);
+	CreateHazard("objBomb", 15, 10);
+	CreateHazard("objRocket", 15, 10);
+	CreateHazard("objGrenade", 15, 10);
+	CreateHazard("objWeight", 15, 10);
+	yield WaitForSeconds(15);
+	
+	CreateHazard("objUfo", 10, 10);
+	CreateHazard("objSmokeGrenade", 1, 9);
+	CreateHazard("objBomb", 15, 10);
+	CreateHazard("objRocket", 15, 10);
+	CreateHazard("objGrenade", 15, 10);
+	CreateHazard("objWeight", 15, 10);
 	yield WaitForSeconds(15);
 	
 }
