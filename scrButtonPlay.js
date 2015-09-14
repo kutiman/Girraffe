121353d05fc2b8b7c734f89a53a539f6e5c128af
kutiman <kutiman@gmail.com>
2015-09-15 02:42:21 +0300

    play button and camera efect

---
+++ scrButtonPlay.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnMouseDown () {
+	
+	GameObject.Instantiate(Resources.Load("Prefabs/objCamEffect"));
+	yield WaitForSeconds(1);
+	Application.LoadLevel("Level");
+}
+
+function CamEffect () {
+	var cam : GameObject = GameObject.FindWithTag("MainCamera");
+	var fromSize : float = 3.2;
+	var duration : float = 1.0;
+	if (cam.GetComponent(Camera).orthographicSize > 0) {
+		cam.GetComponent(Camera).orthographicSize -= (fromSize * Time.deltaTime) / duration;
+	}
+}
---
+++ scrCamEffect.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var duration : float = 0.5;
+var momentum : float = 1.0;
+
+function Start () {
+	yield WaitForSeconds(duration);
+	Destroy(gameObject);
+}
+
+function Update () {
+	CamEffect();
+}
+
+function CamEffect () {
+	var cam : GameObject = GameObject.FindWithTag("MainCamera");
+	var fromSize : float = 3.2;
+
+	if (cam.GetComponent(Camera).orthographicSize > 0) {
+		cam.GetComponent(Camera).orthographicSize += ((fromSize * Time.deltaTime) / duration) * momentum;
+		momentum += Time.deltaTime * 10;
+	}
+}
